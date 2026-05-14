f65ebd3de114680908af8c6d49ec17642c9e9255
possn <pedrossnunes@gmail.com>
2026-05-14 13:26:41 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ucip-v10';
+const CACHE = 'ucip-v11';
 const ASSETS = [
   './', './index.html', './compat_data.js', './manifest.json',
   './apple-touch-icon.png', './icon-192.png', './icon-512.png',
